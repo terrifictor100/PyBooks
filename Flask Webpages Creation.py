1907d2b13f39159539cb67c24bc94197f3e76db6
Victoria Steenson <v.steenson@ymail.com>
2018-03-28 19:56:20 +0100

    made templates work

--- Flask Webpages Creation.py
+++ Flask Webpages Creation.py
@@ -1,22 +1,18 @@
 from flask import Flask, render_template
+import requests
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def homepage():
-    params = {
-        'api_key': 'AIzaSyCbhIgJKBPeTDIhAF4MCY0VXOZoTX3IcAc',
-        'format': 'json'
-    }
-    r = requests.get(
-        "https://www.googleapis.com/books/v1/volumes?q=harrypotter&key=AIzaSyCbhIgJKBPeTDIhAF4MCY0VXOZoTX3IcAc".format(search))
-    return render_template('../templates/homePage.html')
-
-
-@app.route('/cfgbooks/search/')
-def results():
+@app.route('/search')
+def search():
     return render_template('searchResults.html')
+
+
+@app.route('/')
+def home():
+    return render_template('homePage.html')
 
 @app.route('/cfgbooks/search/<specific_book>/')
 def book(specific_book):
