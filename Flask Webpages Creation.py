689d10c7781bb32b4c2949b3c5c4c7ca4581a5b7
Victoria Steenson <v.steenson@ymail.com>
2018-03-28 20:02:29 +0100

    made templates work

--- Flask Webpages Creation.py
+++ Flask Webpages Creation.py
@@ -7,8 +7,32 @@ app = Flask(__name__)
 
 @app.route('/search')
 def search():
-    return render_template('searchResults.html')
+    def find(data):
+        results = []
+        book_data = data.json()
+        books = book_data['items']
 
+        for book in books:
+            result = {}
+            volumeInfo = book.get('volumeInfo')
+            result["title"] = volumeInfo.get('title')
+            result["author"] = volumeInfo.get('authors')[0]
+            result["description"] = volumeInfo.get('description')
+            results.append(result)
+
+        return results
+
+    def search_books(title):
+        r = requests.get(
+            "https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyCbhIgJKBPeTDIhAF4MCY0VXOZoTX3IcAc".format(
+                title))
+        book_list = find(r)
+
+        return book_list
+
+    print(search_books("harry potter"))
+
+    return render_template('searchResults.html', results=book_list)
 
 @app.route('/')
 def home():
