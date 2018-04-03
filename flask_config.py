4c9586b993c1ebd36fbdb1aebc6b3ab0c220b728
Victoria Steenson <v.steenson@ymail.com>
2018-04-03 18:52:06 +0100

    linked html home page and search results page with flask. user input appears on search results page

--- flask_config.py
+++ flask_config.py
@@ -1,45 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 
 app = Flask("PyBooks")
 
 
-@app.route('/search')
-def search():
-    def find(data):
-        results = []
-        book_data = data.json()
-        books = book_data['items']
+@app.route('/search', methods=["POST"])
+def search_books():
+    form_data = request.form
+    search_term = form_data["text"]
+    return search_term
 
-        for book in books:
-            result = {}
-            volumeInfo = book.get('volumeInfo')
-            result["title"] = volumeInfo.get('title')
-            result["author"] = volumeInfo.get('authors')[0]
-            result["description"] = volumeInfo.get('description')
-            results.append(result)
 
-        return results
 
-    def search_books(title):
-        r = requests.get(
-            "https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyCbhIgJKBPeTDIhAF4MCY0VXOZoTX3IcAc".format(
-                title))
-        book_list = find(r)
 
-        return book_list
+    # r = requests.get("https://www.googleapis.com/books/v1/volumes?q=harrypotter&key=AIzaSyCbhIgJKBPeTDIhAF4MCY0VXOZoTX3IcAc")
+    # book_list = search(r)
+    # return book_list
+    # return render_template('searchResults.html', results=book_list)
 
-    print(search_books("harry potter"))
 
-    return render_template('searchResults.html', results=results)
+#def search(data):
+    #results = []
+    #book_data = data.json()
+    #books = book_data['items']
+
+    #for book in books:
+    #result = {}
+    #volumeInfo = book.get('volumeInfo')
+    # result["title"] = volumeInfo.get('title')
+    #   result["author"] = volumeInfo.get('authors')[0]
+    #   result["description"] = volumeInfo.get('description')
+#   results.append(result)
+
 
 @app.route('/')
 def home():
     return render_template('homePage.html')
-
-@app.route('/cfgbooks/search/<specific_book>/')
-def book(specific_book):
-   return render_template('bookDetails.html', specific_book=specific_book)
 
 app.run(debug=True)
